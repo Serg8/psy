f1fcdbf89ef8e8e2667f3682df2f17851b9a6096
Sergey Kulchytski <serg@ib.by>
2020-08-28 19:06:27 +0300

    add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        app init
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <div>logo</div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
