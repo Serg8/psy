8fbd5fdc3caf689d301c9064f28ec1442cae8b78
Sergey Kulchytski <serg@ib.by>
2020-09-03 09:57:37 +0300

    use hooks

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classes from './Footer.module.scss';
 
-function Footer() {
+const Footer = () => {
     return (
         <footer className={classes.footer}>
             FOOTER
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Header.module.scss'
 
-function Header() {
+const Header = () => {
+
+   // let array = useState(false); // это значение не храниться локально, тк. функция не имеет стэйта,
+    // значение хранится на стороне реакта // array = тут только одно значение и одна функция т.е. 2 элемента
+   // let editMode = array[0]; // здесь начальное значение, те false, который мы задали на строке выше
+  //  let setEditMode = array[1]; // здесь функция, которая изменяет первое одиночное значение
+
+    // деструктурированное присваивание
+    let [editMode, setEditMode] = useState(false);
+    //let [status, setStatus] = useState(props.status);  // если нужно, то добавляем ещё один стэйт
+
+    const activateEditMode = () => {
+        setEditMode(true); // setEditMode - это второе значение, которым является функция
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+    }
+
+    // методов жизненного цикла в хуках нет
+
     return (
         <header className={classes.header}>
             <div>logo</div>
@@ -10,6 +30,16 @@ function Header() {
                     <li>Home</li>
                 </ul>
             </nav>
+            <div>
+                {
+                    !editMode &&
+                    <div><span onDoubleClick={ activateEditMode } >botton</span></div>
+                }
+                {
+                    editMode &&
+                    <div><input autoFocus={true} onBlur={ deactivateEditMode } /></div>
+                }
+            </div>
         </header>
     );
 }
